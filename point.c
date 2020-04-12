1b4e97a5bae79c9c5d0fa998893ad2cce1ddf081
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-04-12 18:50:45 +0530

    defined type Point for struct Point

--- point.c
+++ point.c
@@ -1,15 +1,15 @@
 #include <math.h>
 #include "point.h"
 
-unsigned int distance_of_points(struct Point point1, struct Point point2) {
+unsigned int distance_of_points(Point point1, Point point2) {
   int x = pow(point1.x - point2.x, 2);
   int y = pow(point1.y - point2.y, 2);
   return sqrt(x + y);
 }
 
-void get_closest_food(struct Point food_points[], int points_length, struct Point current_location, struct Point *closest_food_location)
+void get_closest_food(Point food_points[], int points_length, Point current_location, Point *closest_food_location)
 {
-  struct Point location = current_location;
+  Point location = current_location;
   unsigned int distance = 100000;
   for(int i = 0; i < points_length; i++) {
     unsigned int temp_distance = distance_of_points(current_location, food_points[i]);
