9114c9e615502123c2464e2bb8e74c18e168d5e5
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-04-12 18:49:18 +0530

    added distance_of_points function

--- point.c
+++ point.c
@@ -1,12 +1,18 @@
 #include <math.h>
 #include "point.h"
 
+unsigned int distance_of_points(struct Point point1, struct Point point2) {
+  int x = pow(point1.x - point2.x, 2);
+  int y = pow(point1.y - point2.y, 2);
+  return sqrt(x + y);
+}
+
 void get_closest_food(struct Point food_points[], int points_length, struct Point current_location, struct Point *closest_food_location)
 {
   struct Point location = current_location;
-  unsigned int distance = 1000000;
+  unsigned int distance = 100000;
   for(int i = 0; i < points_length; i++) {
-    unsigned int temp_distance = sqrt(((current_location.x - food_points[i].x) * (current_location.x - food_points[i].x)) + (current_location.y - food_points[i].y) * (current_location.y - food_points[i].y));
+    unsigned int temp_distance = distance_of_points(current_location, food_points[i]);
     if(distance > temp_distance) {
       distance = temp_distance;
       closest_food_location->x = food_points[i].x;
