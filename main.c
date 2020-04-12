47beae34c64eb97aac74be465d986a10b8232e90
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-04-12 18:40:22 +0530

    added food_points, organism_locations in main

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
+#include <stdio.h>
 #include "point.h"
 
 int main(void)
 {
+  struct Point food_points[5] = {{18, 76}, {19, 66}, {89, 57}, {9, 71}, {55, 38}};
+  struct Point organism_locations[5] = {{18, 86}, {97, 27}, {69, 7}, {10, 94}, {36, 27}};
+  struct Point closest_food_location = {0, 0};
+  for(int i = 0; i < 5; i++) {
+    get_closest_food(food_points, 5, organism_locations[i], &closest_food_location);
+    printf("Location of organism: [%d %d],  Closest food target: [%d %d]\n", organism_locations[i].x, organism_locations[i].y, closest_food_location.x, closest_food_location.y);
+  }
   return 0;
 }
