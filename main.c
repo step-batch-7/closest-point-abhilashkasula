6f7013316bc9310f59c94b5b342c515a1c605780
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-04-12 19:04:03 +0530

    defined FOOD_POINTS, ORGANISM_LOCATIONS, LENGTH

--- main.c
+++ main.c
@@ -3,11 +3,11 @@
 
 int main(void)
 {
-  Point food_points[5] = {{18, 76}, {19, 66}, {89, 57}, {9, 71}, {55, 38}};
-  Point organism_locations[5] = {{18, 86}, {97, 27}, {69, 7}, {10, 94}, {36, 27}};
+  Point food_points[LENGTH] = FOOD_POINTS;
+  Point organism_locations[LENGTH] = ORGANISM_LOCATIONS;
   Point closest_food_location = {0, 0};
-  for(int i = 0; i < 5; i++) {
-    get_closest_food(food_points, 5, organism_locations[i], &closest_food_location);
+  for(int i = 0; i < LENGTH; i++) {
+    get_closest_food(food_points, LENGTH, organism_locations[i], &closest_food_location);
     printf("Location of organism: [%d %d],  Closest food target: [%d %d]\n", organism_locations[i].x, organism_locations[i].y, closest_food_location.x, closest_food_location.y);
   }
   return 0;
