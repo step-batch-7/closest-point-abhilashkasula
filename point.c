86dd7f98697d69c2f79c91d6ff06bea6c1701c25
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-04-13 13:11:11 +0530

    simplified updating closest_food_location in get_closest_food

--- point.c
+++ point.c
@@ -14,8 +14,7 @@ void get_closest_food(Point food_points[], int points_length, Point current_loca
     double distance_to_current_food = distance_of_points(current_location, food_points[i]);
     if(distance > distance_to_current_food) {
       distance = distance_to_current_food;
-      closest_food_location->x = food_points[i].x;
-      closest_food_location->y = food_points[i].y;
+      *closest_food_location = food_points[i];
     }
   }
 }
