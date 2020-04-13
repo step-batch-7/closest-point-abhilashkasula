92b0b3682ef8c8ce8ca930646ee42ab2e0901084
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-04-13 13:16:40 +0530

    removed macro MAX_DISTANCE

--- point.c
+++ point.c
@@ -9,8 +9,9 @@ double distance_of_points(Point point1, Point point2) {
 
 void get_closest_food(Point food_points[], int points_length, Point current_location, Point *closest_food_location)
 {
-  double distance = MAX_DISTANCE;
-  for(int i = 0; i < points_length; i++) {
+  double distance = distance_of_points(current_location, food_points[0]);
+  *closest_food_location = food_points[0];
+  for(int i = 1; i < points_length; i++) {
     double distance_to_current_food = distance_of_points(current_location, food_points[i]);
     if(distance > distance_to_current_food) {
       distance = distance_to_current_food;
